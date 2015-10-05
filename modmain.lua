ae881d1df9b38baf898851c03a6de49454c78e45
Botmasher <languagejosh@hotmail.com>
2015-10-04 22:39:17 -0400

    Add keypress handler test function to modmain

--- modmain.lua
+++ modmain.lua
@@ -1,13 +1,26 @@
--- local variables
--- if in the mod environment (modmain.lua), you need to get require from the global environment
+-- this is the mod environment; get require from the global environment
 --local require = GLOBAL.require
  
 -- reference to the cached result stored in package.loaded["path/file"]
 --local Cooking = require "cooking"
 
-local function TwigsCookLikeFruit()
+local key1 = GLOBAL.KEY_R
+
+local function TwigsCookLikeFruit ()
     AddIngredientValues ({"twigs"}, {fruit=1})
 end
- 
-AddSimPostInit(TwigsCookLikeFruit)
-AddSimPostInit(function() print ("This thing is running SAUCESTYLE!!!") end)
+
+local function ActionOnKeyDown ()
+	GLOBAL.TheInput:AddKeyDownHandler (key1, function ()
+		print ("Pressed it! You sooo pressed the key!")	
+		if not GLOBAL.IsPaused() then
+			print ("\nNot pressed when paused.")
+		else
+			print ("\nYes, paused when pressed.")
+		end
+	end)
+end
+
+AddSimPostInit (TwigsCookLikeFruit)
+AddSimPostInit (ActionOnKeyDown)
+AddSimPostInit (function() print ("This thing is running like a SAUCE!!!") end)
